a643731f768ceacf6bd03c1693cc7f31768279a5
andyxu723 <xudongqi2009@gmail.com>
2016-06-18 21:21:11 +0800

    first commit

---
+++ article/views.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python env
+#coding:utf8
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+
+# Create your views here.
+
+#def home(request):
+#    return HttpResponse("Hello World, Django")
+
+def home(request):
+    post_list = Article.objects.all()  #获取全部的Article对象
+    return render(request, 'home.html', {'post_list' : post_list})
+
+def detail(request,my_args):
+#    return HttpResponse("You are looking for my_args %s." % my_args)
+#    return HttpResponse("You're looking at my_args %s." % my_args)
+    post = Article.objects.all()[int(my_args)]
+    str = ("title = %s, category = %s, date_time = %s, content = %s"
+        % (post.title, post.category, post.date_time, post.content))
+    return HttpResponse(str)
+
+def test(request) :
+    return render(request, 'test.html', {'current_time': datetime.now()})
